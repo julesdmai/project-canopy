7677f7677e96035bced26865eea48884b317ff9b
julesdmai <julesdmai@gmail.com>
2024-11-07 10:31:11 -0800

    UI changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello this is Jules
+          Project Canopy
         </p>
       </header>
     </div>
